7c31296818734762b582d28836422f94b40a8060
wylupek <116514695+wylupek@users.noreply.github.com>
2024-05-14 15:34:20 +0200

    saving code and text

--- lab6 - lossless compression LZW/main.py
+++ lab6 - lossless compression LZW/main.py
@@ -12,27 +12,19 @@ def create(data: list) -> dict:
             for i, item in enumerate(freq_dict)}
 
 
-def decodeOld(encoded_text, code) -> str:
-    code_inv = {v: k for k, v in code.items()}
-    decoded_text = ""
-    current_code = ""
-    for bit in encoded_text:
-        current_code += '1' if bit else '0'
-        if current_code in code_inv:
-            decoded_text += code_inv[current_code]
-            current_code = ""
-    return decoded_text
-
-
-def saveOld(filename, code, encoded_text):
+def save(filename: str, list_of_codes: list, codes: dict) -> None:
     with open(filename + ".bin", 'wb') as file:
+        encoded_text = ba.bitarray()
+        for code in list_of_codes:
+            encoded_text.extend(ba.bitarray(codes[code]))
         encoded_text.tofile(file)
 
+    codes["len"] = len(encoded_text)
     with open(filename + "_code.json", 'w') as file:
-        json.dump(code, file)
+        json.dump(codes, file)
 
 
-def loadOld(filename):
+def load(filename):
     with open(filename + ".bin", 'rb') as file:
         encoded_text = ba.bitarray()
         encoded_text.fromfile(file)
@@ -43,39 +35,97 @@ def loadOld(filename):
     return code, encoded_text
 
 
-def load(file_path) -> ba.bitarray:
+def load_file(file_path) -> ba.bitarray:
     bit_array = ba.bitarray()
     with open(file_path, 'rb') as file:
         bit_array.fromfile(file)
     return bit_array
 
 
-def encode(bit_array: ba.bitarray) -> list:
+def encode(bit_array: ba.bitarray, code_size: float) -> list:
     output = []
-    lzw_code = ['0', '1']
+    lzw_code = {'0': 0, '1': 1}
+    next_code = 2
     s = str(bit_array[0])
     for c in bit_array.to01()[1:]:
-        x = ''.join([s, c])
+        x = s + c
         if x in lzw_code:
             s = x
         else:
-            output.append(lzw_code.index(s))
-            lzw_code.append(x)
+            output.append(str(lzw_code[s]))
+            if next_code < code_size:
+                lzw_code[x] = next_code
+                next_code += 1
             s = c
-    output.append(lzw_code.index(s))
+    output.append(str(lzw_code[s]))
+    return output
+
+
+def decode(bit_array: ba.bitarray, code, code_size: float):
+    code_inv = {v: int(k) for k, v in list(code.items())[:-1]}
+    decoded_codes = []
+    current_code = ""
+    try:
+        for i in range(code["len"]):
+            current_code += '1' if bit_array[i] else '0'
+            if current_code in code_inv:
+                decoded_codes.append(code_inv[current_code])
+                current_code = ""
+    except KeyError:
+        for bit in bit_array:
+            current_code += '1' if bit else '0'
+            if current_code in code_inv:
+                decoded_codes.append(code_inv[current_code])
+                current_code = ""
+
+    output = []
+    lzw_code = {0: '0', 1: '1'}
+    next_code = 2
+    c = ''
+    old = decoded_codes[0]
+    output.append(lzw_code[old])
+
+    for new in decoded_codes[1:]:
+        if new in lzw_code:
+            word = lzw_code[new]
+        else:
+            word = lzw_code[old] + c
+        output.append(word)
+        c = word[0]
+        if next_code < code_size:
+            lzw_code[next_code] = lzw_code[old] + c
+            next_code += 1
+        old = new
     return output
 
 
 if __name__ == '__main__':
-    my_filename = "../data/norm_wiki_en.txt"
-    my_bit_array = load(my_filename)
-    # my_bit_array = ba.bitarray("101001000")
-    encoded_data = encode(my_bit_array)
-    print(encoded_data)
-    code = create(encoded_data)
-    print(code)
+    my_code_size = 2**12
+    my_filename = "../data/lena.bmp"
+
+    # Load -> Encode -> Save
+    my_bit_array = load_file(my_filename)
+    encoded_data = encode(my_bit_array, my_code_size)
+    my_code = create(encoded_data)
+    save(my_filename, encoded_data, my_code)
+    print("Loaded file len:", len(my_bit_array))
+
+    # Load -> Decode -> Compare
+    my_code_loaded, my_encoded_text_loaded = load(my_filename)
+    decoded_data = ''.join(decode(my_encoded_text_loaded, my_code_loaded, my_code_size))
+    print("Decoded data len:", len(decoded_data))
+    print("If files are the same:", decoded_data == my_bit_array.to01())
 
 
+'''
+filename                    original    'inf'           2**12       2**18
+norm_wiki_sample.txt        10537       6233+67074      8974+95     6560+8059
+wiki_sample.txt             11630       7391+77434      10441+95    7606+8047
+lena.bmp                    11255       10522+104620    12186+95    11373+8030
 
+'''
+
+
+# my_bit_array = ba.bitarray("101001000")
 # 1 0 2 3 0 6
 # 1 0 4 2 3 0 2
